08c9998aedfce54c8c409ffea81ca71aa666a957
coreb2c <abdullah.tulek@coreb2c.com>
2017-10-11 11:07:34 +0300

    initial upload

---
+++ traits/ModuleTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace coreb2c\auth\traits;
+
+use coreb2c\auth\Module;
+
+/**
+ * Trait ModuleTrait
+ * @property-read Module $module
+ * @package coreb2c\auth\traits
+ */
+trait ModuleTrait
+{
+    /**
+     * @return Module
+     */
+    public function getModule()
+    {
+        return \Yii::$app->getModule('user');
+    }
+}
