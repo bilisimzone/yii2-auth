3a9208a16c0bc3b6cf1db23e3822d4af287ce9e3
coreb2c <abdullah.tulek@coreb2c.com>
2017-12-03 20:17:48 +0300

    user component added

---
+++ components/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace coreb2c\auth\components;
+
+class User extends \yii\web\User {
+
+    public function init() {
+        parent::init();
+    }
+
+    /**
+     * @var string the class name of the [[identity]] object.
+     */
+    public $identityClass;
+
+    /**
+     * Checks if the user can perform the operation as specified by the given permission.
+     *
+     * Note that you must configure "authManager" application component in order to use this method.
+     * Otherwise it will always return false.
+     *
+     * @param string $permissionName the name of the permission (e.g. "edit post") that needs access check.
+     * @param array $params name-value pairs that would be passed to the rules associated
+     * with the roles and permissions assigned to the user.
+     * @param bool $allowCaching whether to allow caching the result of access check.
+     * When this parameter is true (default), if the access check of an operation was performed
+     * before, its result will be directly returned when calling this method to check the same
+     * operation. If this parameter is false, this method will always call
+     * [[\yii\rbac\CheckAccessInterface::checkAccess()]] to obtain the up-to-date access result. Note that this
+     * caching is effective only within the same request and only works when `$params = []`.
+     * @return bool whether the user can perform the operation as specified by the given permission.
+     */
+    public function can($permissionName, $params = [], $allowCaching = true) {
+        if (\Yii::$app->user->identity->isAdmin) {
+            return true;
+        }
+        return parent::can($permissionName, $params, $allowCaching);
+    }
+
+}
